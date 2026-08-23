a119126df9de6b5df63fb7d7583207e0952cd79f
krishnakarale-10 <karalekrishna10@gmail.com>
2026-08-23 23:32:09 +0530

    deleted cleanup.js

--- cleanup.js
+++
@@ -1,1 +0,0 @@
-import prisma from './src/config/prisma.js'; prisma.user.deleteMany({where:{email:'karalekrishna0@gmail.com'}}).then(()=>console.log('done')).catch(e=>console.log(e)).finally(()=>process.exit(0));
